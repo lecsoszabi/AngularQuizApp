ff5723c41f0f9251594ba4ed6ccdde53301ce92d
lecsoszabi <ado.szabi@gmail.com>
2025-03-25 19:31:39 +0100

    feat: update application structure and routing configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RegisterComponent } from './components/register/register.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthService } from './services/auth.service';
+import {AsyncPipe, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: true,
-  imports: [RegisterComponent, QuizComponent]
+  imports: [
+    AsyncPipe,
+    QuizComponent,
+    LoginComponent,
+    RegisterComponent,
+    NgIf
+  ],
+  standalone: true
 })
 export class AppComponent {
-  title = 'quiz-app';
+  title = "quiz-app";
+  constructor(public authService: AuthService) {}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { QuizComponent } from './components/quiz/quiz.component';
-import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: RegisterComponent },
-  { path: 'quiz', component: QuizComponent }
+  { path: '', component: LoginComponent },
+  { path: 'quiz', component: QuizComponent, canActivate: [AuthGuard] }
 ];
