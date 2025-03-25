aed783c7c9b14877f1c83a176de14a83b9da1e87
lecsoszabi <ado.szabi@gmail.com>
2025-03-25 14:29:34 +0100

    feat: implement routing for the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { QuizComponent } from './components/quiz/quiz.component';
+import { RegisterComponent } from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RegisterComponent },
+  { path: 'quiz', component: QuizComponent }
+];
