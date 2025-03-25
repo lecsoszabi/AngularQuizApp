45aefdd283585582377e3eea4ebd74c109ef2414
lecsoszabi <ado.szabi@gmail.com>
2025-03-25 14:28:42 +0100

    feat: add base application component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [RegisterComponent, QuizComponent]
 })
 export class AppComponent {
   title = 'quiz-app';
