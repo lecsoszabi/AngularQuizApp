7ec766a848061707e396ad9ac4a09cd297fd8d99
lecsoszabi <159720348+lecsoszabi@users.noreply.github.com>
2025-05-21 17:30:17 +0200

    Delete src/app/environments directory

--- src/app/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
- export const environment = {
-  production: true,
-  apiUrl: 'https://opentdb.com/api.php?amount=10&type=multiple'
-};
--- src/app/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://opentdb.com/api.php?amount=10&type=multiple'
-};
